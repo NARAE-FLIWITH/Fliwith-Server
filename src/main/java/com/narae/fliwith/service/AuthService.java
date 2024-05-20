5b99b9810063fb190ae3fe08b670e7729ac45860
jiyoon <younggy123@naver.com>
2024-05-21 01:30:19 +0900

    feat: 이메일 인증 확인

--- src/main/java/com/narae/fliwith/service/AuthService.java
+++ src/main/java/com/narae/fliwith/service/AuthService.java
@@ -3,6 +3,7 @@ package com.narae.fliwith.service;
 import com.narae.fliwith.domain.SignupStatus;
 import com.narae.fliwith.domain.User;
 import com.narae.fliwith.exception.user.LogInFailException;
+import com.narae.fliwith.exception.user.NotFoundUserException;
 import com.narae.fliwith.exception.user.RequireEmailAuthException;
 import com.narae.fliwith.repository.UserRepository;
 import jakarta.transaction.Transactional;
@@ -21,5 +22,12 @@ public class AuthService {
             throw new RequireEmailAuthException();
         }
         return user;
+    }
+
+    public void checkAuthEmail(String email){
+        User user = userRepository.findByEmail(email).orElseThrow(NotFoundUserException::new);
+        if(!user.getSignupStatus().equals(SignupStatus.COMPLETE)){
+            throw new RequireEmailAuthException();
+        }
     }
 }
